769f7a61e8e783e706ff5b869f7c30234993042d
kevin <Kevinwburns@gmail.com>
2017-02-11 17:21:35 -0500

    added new html content

--- EPermitting/js/submit-event.js
+++ EPermitting/js/submit-event.js
@@ -9,3 +9,23 @@
     document.getElementById('nameField').textContent = msg;
   });
 }());
+
+
+
+
+var applicantName; //first name
+var blockNum; //last name
+function getApplicantName() {  
+    return document.getElementById("nameField").value;
+}
+function getBlockNum()    {
+    return document.getElementById("blockNumField").value;
+}
+
+function display()  {
+    applicantName = getApplicantName();
+    blockNum = getBlockNum();
+    window.alert(applicantName + " " + blockNum);
+    document.getElementById("theWholeForm").submit();
+    
+}
--- EPermitting/js/utilities.js
+++ EPermitting/js/utilities.js
@@ -12,10 +12,10 @@ function addEvent(el, event, callback) {
 }
 
 //Adds user-input controls for File number format
-var myFileNum = document.fileNumField.fileNumField;
+var myFileNum = document.wholeForm.fileNumField;
 var myFileNumError = document.getElementById("formError");
 
-myFileNum.onchange = function() {
+myFileNum.onchange = function checkFileNum() {
   var myFilePattern = new RegExp("\\d{4}[\\-]\\d{2}[\\-]\\d{4}[\\.]\\d{1}", "i");
   var isValid = this.value.search(myFilePattern) >= 0;
 //Checks to see if the File number entered is in the proper format
@@ -23,7 +23,7 @@ if (!(isValid)) {
   myFileNumError.innerHTML = "Input does not match expected pattern. xxxx-xx-xxxx.x";
 } else if (isValid) {
   myFileNumError.innerHTML = "";
-}
+} 
 
 }
 
@@ -33,49 +33,116 @@ document.wholeForm.nameField.onfocus = function() {
   document.getElementById('myNameHint').innerHTML = "(Enter first name, then last)";
 }
 
+
 //Removes user-input guidance for Applicant Name format
 document.wholeForm.nameField.onblur = function() {
   document.getElementById('myNameHint').innerHTML = "";
 }
 
+
 //Adds user-input guidance for Application File Number format
-document.fileNumField.fileNumField.onfocus = function() {
+document.wholeForm.fileNumField.onfocus = function() {
   document.getElementById('myFileHint').innerHTML = "(e.g., 1234-56-7890.12)";
 }
 
 //Removes user-input guidance for Application File Number format
-document.fileNumField.fileNumField.onblur = function() {
+document.wholeForm.fileNumField.onblur = function() {
   document.getElementById('myFileHint').innerHTML = "";
 }
 
 
 //Adds user-input guidance for project site Block Number format
-document.blockNum.blockNumField.onfocus = function() {
+document.wholeForm.blockNumField.onfocus = function() {
   document.getElementById('myBlockHint').innerHTML = "(Enter Block(s) number(s), separated by commas, or select N/A)";
 }
 
 //Removes user-input guidance for project site Block Number format
-document.blockNum.blockNumField.onblur = function() {
+document.wholeForm.blockNumField.onblur = function() {
   document.getElementById('myBlockHint').innerHTML = "";
 }
 
+
 //Adds user-input guidance for project site Lot Number format
-document.lotNum.lotNumField.onfocus = function() {
+document.wholeForm.lotNumField.onfocus = function() {
   document.getElementById('myLotHint').innerHTML = "(Enter Block(s) number(s), separated by commas, or select N/A)";
 }
 
 //Removes user-input guidance for project site Lot Number format
-document.lotNum.lotNumField.onblur = function() {
+document.wholeForm.lotNumField.onblur = function() {
   document.getElementById('myLotHint').innerHTML = "";
 }
 
 
 //Adds user-input guidance for Watershed Management field
-document.wtrshdMgtField.watershedManagement.onfocus = function() {
+document.wholeForm.watershedManagement.onfocus = function() {
   document.getElementById('myWtrshdMgtHint').innerHTML = "(Enter name of watershed management, or select N/A)";
 }
 
 //Removes user-input guidance for Watershed Management field
-document.wtrshdMgtField.watershedManagement.onblur = function() {
+document.wholeForm.watershedManagement.onblur = function() {
   document.getElementById('myWtrshdMgtHint').innerHTML = "";
 }
+
+
+//Adds user-input guidance for Watershed field
+document.wholeForm.watershedField.onfocus = function() {
+  document.getElementById('myWtrshdHint').innerHTML = "(Enter name of watershed, or select N/A)";
+}
+
+//Removes user-input guidance for Watershed field
+document.wholeForm.watershedField.onblur = function() {
+  document.getElementById('myWtrshdHint').innerHTML = "";
+}
+
+
+//Adds user-input guidance for Sub-watershed field
+document.wholeForm.subWatershedField.onfocus = function() {
+  document.getElementById('mySubWtrshdHint').innerHTML = "(Enter name of sub-watershed, or select N/A)";
+}
+
+//Removes user-input guidance for Sub-watershed field
+document.wholeForm.subWatershedField.onblur = function() {
+  document.getElementById('mySubWtrshdHint').innerHTML = "";
+}
+
+
+//Adds user-input guidance for Waterway field
+document.wholeForm.waterwayField.onfocus = function() {
+  document.getElementById('myWaterwayHint').innerHTML = "(Enter name of waterway, or select N/A)";
+}
+
+//Removes user-input guidance for Waterway field
+document.wholeForm.waterwayField.onblur = function() {
+  document.getElementById('myWaterwayHint').innerHTML = "";
+}
+
+
+//Adds user-input guidance for Resource Value Classification field
+document.wholeForm.rVCField.onfocus = function() {
+  document.getElementById('myRVCNAHint').innerHTML = "(Enter the resource value classification, or select N/A)";
+}
+
+//Removes user-input guidance for Resource Value Classification field
+document.wholeForm.rVCField.onblur = function() {
+  document.getElementById('myRVCNAHint').innerHTML = "";
+}
+
+
+//Adds user-input guidance for Tidelands Claim # field
+document.wholeForm.claimNum.onfocus = function() {
+  document.getElementById('myTidalClaimHint').innerHTML = "(Enter the tidal claim number, or select N/A)";
+}
+
+//Removes user-input guidance for Tidelands Claim # field
+document.wholeForm.claimNum.onblur = function() {
+  document.getElementById('myTidalClaimHint').innerHTML = "";
+}
+
+
+function clearAndShow(){
+  var message = document.getElementById("nameField").innerHTML = document.getElementById("display_message").value;
+}
+
+
+
+
