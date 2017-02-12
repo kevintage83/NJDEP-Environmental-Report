170e6803ead6a08c40ed64ab31c0d9f0c4a05fc7
kevin <Kevinwburns@gmail.com>
2017-02-11 20:23:14 -0500

    made the header links open in new windows

--- EPermitting/js/submit-event.js
+++ EPermitting/js/submit-event.js
@@ -29,3 +29,39 @@ function display()  {
     document.getElementById("theWholeForm").submit();
     
 }
+
+
+function openDEPHome() {
+    window.open("http://www.nj.gov/dep/");
+}
+
+
+function openCZM() {
+    window.open("http://www.nj.gov/dep/rules/rules/njac7_7.pdf");
+}
+
+
+function openFWW() {
+    window.open("http://www.nj.gov/dep/rules/rules/njac7_7a.pdf");
+}
+
+
+function openSWM() {
+    window.open("http://www.nj.gov/dep/rules/rules/njac7_8.pdf");
+}
+
+
+function openSWQ() {
+    window.open("http://www.nj.gov/dep/rules/rules/njac7_9b.pdf");
+}
+
+
+function openFHA() {
+    window.open("http://www.nj.gov/dep/rules/rules/njac7_13.pdf");
+}
+
+
+function openWPCA() {
+    window.open("http://www.nj.gov/dep/rules/rules/njac7_14.pdf");
+}
+
